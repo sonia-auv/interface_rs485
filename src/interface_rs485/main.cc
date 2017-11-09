99a3ca0c248941194708f5a399f233b4c67e2809
LMongrain <xeaprog@gmail.com>
2017-11-09 15:56:52 -0500

    adding main

--- src/interface_rs485/main.cc
+++ src/interface_rs485/main.cc
@@ -1,7 +1,10 @@
-#include <stdlib.h>
 #include <ros/ros.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    ros::init(argc, argv, "interface_rs485");
 
+    ros::NodeHandlePtr nh(new ros::NodeHandle("~"));
+    interface_rs485::InterfaceRs485Node interface_node{nh};
+    interface_node.Spin();
 }
