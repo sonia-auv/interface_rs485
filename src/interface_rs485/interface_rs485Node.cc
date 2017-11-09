0c7382b9f397d94c41f3d4254af1d9877fde539a
LMongrain <xeaprog@gmail.com>
2017-11-09 15:56:34 -0500

    add interface_rs485Node skeleton

---
+++ src/interface_rs485/interface_rs485Node.cc
@@ -0,0 +1,87 @@
+#include "InterfaceRs485Node.h"
+#include <ros/ros.h>
+#include <thread>
+#include <mutex>
+
+
+namespace interface_rs485
+{
+
+    // node Construtor
+    InterfaceRs485Node::InterfaceRs485Node(const ros::NodeHandlePtr &_nh) :
+    nh(nh)
+    {
+        serialConnection = Serial("/dev/ttyS5");
+        ros::Publisher transmiter = nh->advertise<interface_rs485::SendRS485Msg>("/interface_rs485/dataTx", 100);
+        ros::Suscriber receiver = nh->subscribe("/interface_rs485/dataRx", 100, InterfaceRs485Node::receiveData);
+    }
+
+    // node destructor
+    InterfaceRs485Node::~InterfaceRs485Node()
+    {
+        receiver.shutdown();
+    }
+
+    // node spin
+    void InterfaceRs485Node::Spin()
+    {
+        ros::Rate r(100);
+        while(ros::ok())
+        {
+            ros::spinOnce();
+
+            std::thread reader(InterfaceRs485Node::readData);
+            std::thread writer(InterfaceRs485Node::writeData);
+
+            reader.join();
+            writer.join();
+
+            r.sleep();
+        }
+    }
+
+    void InterfaceRs485Node::transmitData()
+    {
+    }
+
+    //callback when the subscriber receive data
+    void InterfaceRs485Node::receiveData(const interface_rs485::ConstPtr &receivedData)
+    {
+        writerQueue.push(receivedData);
+    }
+
+    void InterfaceRs485Node::readData()
+    {
+        while(!ros::isShuttingDown())
+        {
+            
+        }
+    }
+
+    // write the data in a message
+    void InterfaceRs485Node::writeData()
+    {
+        while(!ros::isShuttingDown())
+        {
+            if(!writerQueue.empty())
+            {
+                interface_rs485::ConstPtr msg_ptr = writerQueue.pop();
+                writeCount += 1;
+                if(writeCount >= std::numeric_limits<int>::max() - 2)
+                {
+                    writeCount = 0;
+                }
+
+                ROS_DEBUG("data in msg number %d: %s", writeCount, InterfaceRs485Node::printMsg(msg_ptr).c_str());
+            }
+        }
+    }
+
+    // return a std::string to print
+    std::string InterfaceRs485Node::printMsg(interface_rs485::ConstPtr &msg_ptr)
+    {
+        std::stringstream s;
+        s << "slave: " << msg_ptr->slave << "/cmd: " << msg_ptr->cmd << "/data: " << msg_ptr->data << std::endl;
+        return s.str();
+    }
+}
---
+++ src/interface_rs485/interface_rs485Node.h
@@ -0,0 +1,41 @@
+#ifndef INTERFACE_RS485_NODE_H
+#define INTERFACE_RS485_NODE_H
+
+#include <interface_rs485/SendRS485Msg.h>
+#include "../driver/serial.h"
+#include <queue>
+#include <string>
+
+namespace interface_rs485
+{
+    class InterfaceRs485Node
+    {
+    public:
+
+        InterfaceRs485Node(const ros::NodeHandlePtr &_nh);
+        ~InterfaceRs485Node();
+
+        void Spin();
+
+    private:
+
+        void transmitData();
+        void receiveData();
+        void readData();
+        void writeData();
+
+        std::string printMsg();
+
+        int writeCount = 0;
+        int readCount = 0;
+
+        ros::NodeHandlePtr nh;
+        Serial serialConnection;
+
+        std::queue<interface_rs485::ConstPtr> writerQueue;
+
+        ros::Time timestamp
+    };
+}
+
+#endif
