2d5366b09cc9cbaf9213d5c6447e1aa1079c46a0
Kam0ss <camille.sauvain.1@etsmtl.net>
2022-04-03 23:06:31 -0400

    changed env variable for LOCAL_AUV, need change in deploy

--- src/interface_rs485_sim.py
+++ src/interface_rs485_sim.py
@@ -2,6 +2,7 @@
 import rospy
 import numpy
 import struct
+import os
 
 from sonia_common.msg import *
 
@@ -15,28 +16,21 @@ def sim_power_auv7():
         for i in range(0, 4):
             msg.slave = i
             msg.cmd = 0
-            msg.data = voltage_generation()
-            print(msg)
+            msg.data = voltage_generation_AUV7()
             pub.publish(msg)
 
             msg.cmd = 1
-            msg.data = current_generation()
-            print(msg)
+            msg.data = current_generation(3)
             pub.publish(msg)
 
             msg.cmd = 15
-            msg.data = [numpy.random.randint(0,2),numpy.random.randint(0,2)]
-            print(msg)
-            pub.publish(msg)
-
-            msg.cmd = 15
-            msg.data = motor_generation()
+            msg.data = motor_generation(2)
             pub.publish(msg)
 
         rate.sleep()
 
 
-def voltage_generation():
+def voltage_generation_AUV7():
     rand = []
     for j in range(0,3):
         rand.append(numpy.random.uniform(14.4, 16.8))
@@ -44,17 +38,22 @@ def voltage_generation():
 
     return transform(rand)
 
-
-def current_generation():
+def voltage_generation_AUV8():
     rand = []
-    for j in range(0,3):
+    for j in range (0,10):
+        rand.append(numpy.random.uniform(14.4, 16.8))
+    return transform(rand)
+
+def current_generation(nb):
+    rand = []
+    for j in range(0,nb):
         rand.append(numpy.random.uniform(0, 25))
 
     return transform(rand)
 
-def motor_generation():
+def motor_generation(nb):
     rand = []
-    for j in range(0,8):
+    for j in range(0,nb):
         rand.append(numpy.random.randint(3))
     return rand
 
@@ -69,10 +68,37 @@ def transform(rand):
     return data
 
 
+def sim_power_auv8():
+    rospy.init_node('interface_rs485_simulation', anonymous=True)
+    pub = rospy.Publisher('/interface_rs485/dataTx', SendRS485Msg, queue_size=100)
+    rate = rospy.Rate(1)  # 10hz
+    msg = SendRS485Msg()
+    while not rospy.is_shutdown():
+        msg.slave = 8
+        msg.cmd = 0
+        msg.data = voltage_generation_AUV8()
+        pub.publish(msg)
+
+        msg.cmd = 1
+        msg.data = current_generation(10)
+        pub.publish(msg)
+
+        msg.cmd = 15
+        msg.data = motor_generation(8)
+        pub.publish(msg)
+
+        rate.sleep()
+
+
 
 if __name__ == '__main__':
     try:
-        sim_power_auv7()
+        auv = os.getenv('LOCAL_AUV',"AUV7")
+        print(auv)
+        if (auv=="AUV7"):
+            sim_power_auv7()
+        else:
+            sim_power_auv8()
     except rospy.ROSInterruptException:
         pass
 
