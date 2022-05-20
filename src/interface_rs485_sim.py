cda71234d5ef96c375074dd5152fc52fa8ad131c
Kam0ss <Kam0ss>
2022-05-20 09:30:17 -0400

    changed AUV7 simulation data order

--- src/interface_rs485_sim.py
+++ src/interface_rs485_sim.py
@@ -6,35 +6,12 @@ import os
 
 from sonia_common.msg import *
 
-
-def sim_power_auv7():
-    rospy.init_node('interface_rs485_simulation', anonymous=True)
-    pub = rospy.Publisher('/interface_rs485/dataTx', SendRS485Msg, queue_size=100)
-    rate = rospy.Rate(1)  # 10hz
-    msg = SendRS485Msg()
-    while not rospy.is_shutdown():
-        for i in range(0, 4):
-            msg.slave = i
-            msg.cmd = 0
-            msg.data = voltage_generation_AUV7()
-            pub.publish(msg)
-
-            msg.cmd = 1
-            msg.data = current_generation(3)
-            pub.publish(msg)
-
-            msg.cmd = 15
-            msg.data = motor_generation(2)
-            pub.publish(msg)
-
-        rate.sleep()
-
-
 def voltage_generation_AUV7():
     rand = []
-    for j in range(0,3):
-        rand.append(numpy.random.uniform(14.4, 16.8))
+    rand.append(numpy.random.uniform(14.4, 16.8))
+    rand.append(numpy.random.uniform(14.4, 16.8))
     rand.append(numpy.random.uniform(11.5,13))
+    rand.append(numpy.random.uniform(14.4, 16.8))
 
     return transform(rand)
 
@@ -67,6 +44,27 @@ def transform(rand):
 
     return data
 
+def sim_power_auv7():
+    rospy.init_node('interface_rs485_simulation', anonymous=True)
+    pub = rospy.Publisher('/interface_rs485/dataTx', SendRS485Msg, queue_size=100)
+    rate = rospy.Rate(1)  # 10hz
+    msg = SendRS485Msg()
+    while not rospy.is_shutdown():
+        for i in range(0, 4):
+            msg.slave = i
+            msg.cmd = 0
+            msg.data = voltage_generation_AUV7()
+            pub.publish(msg)
+
+            msg.cmd = 1
+            msg.data = current_generation(3)
+            pub.publish(msg)
+
+            msg.cmd = 15
+            msg.data = motor_generation(2)
+            pub.publish(msg)
+
+        rate.sleep()
 
 def sim_power_auv8():
     rospy.init_node('interface_rs485_simulation', anonymous=True)
@@ -94,10 +92,10 @@ def sim_power_auv8():
 if __name__ == '__main__':
     try:
         auv = os.getenv('LOCAL_AUV',"AUV7")
-        print(auv)
+        print("LOCAL_AUV set on : " + auv)
         if (auv=="AUV7"):
             sim_power_auv7()
-        else:
+        elif (auv=="AUV8"):
             sim_power_auv8()
     except rospy.ROSInterruptException:
         pass
